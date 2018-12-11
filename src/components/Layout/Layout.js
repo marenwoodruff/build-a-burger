242ee5c4a60f018cc6c81bb6d5d99a5d6b0d9dcf
MarenWoodruff <wren403@gmail.com>
2018-12-11 11:38:06 -0500

    added burger builder component + basic css style for the layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import classes from './Layout.css';
 import Aux from '../../hoc/Aux';
 
 const layout = (props) => (
   <Aux>
     <div>Toolbar, Sidebar, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
