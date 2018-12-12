3f49c21aca5cc008c8d21b88c23a6d2463644025
MarenWoodruff <wren403@gmail.com>
2018-12-12 16:26:18 -0500

    able to add/subtract ingredients from buger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  {label: 'gem lettuce', type: 'salad'},
+  {label: 'beecher\'s cheddar', type: 'cheese'},
+  {label: 'veggie bacon', type: 'bacon'},
+  {label: 'beyond burger', type: 'meat'}
+];
+
 const buildControls = (props) => {
   return (
-    <div></div>
+    <div className={classes.BuildControls}>
+      <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+      {controls.map(ctrl => (
+        <BuildControl 
+          key={ctrl.label} 
+          label={ctrl.label} 
+          added={() => props.ingredientAdded(ctrl.type)}
+          deleted={() => props.ingredientDeleted(ctrl.type)} 
+          disabled={props.disabled[ctrl.type]} />
+      ))}
+      <button>order now</button>
+    </div>
   )
 }
 
