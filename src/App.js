1c8edc3a2d006c3666fa699907026cef01294872
MarenWoodruff <wren403@gmail.com>
2019-01-10 11:49:23 -0800

    working on authentication/

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Layout from './components/Layout/Layout';
+import Auth from './containers/Auth/Auth';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Layout from './components/Layout/Layout';
 import Orders from './containers/Orders/Orders';
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
             <Route path="/" component={BurgerBuilder} />
           </Switch>
         </Layout>
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,8 +5,9 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-    <NavigationItem link="/orders">Orders</NavigationItem>
+    <NavigationItem link="/" exact>burger builder</NavigationItem>
+    <NavigationItem link="/orders">orders</NavigationItem>
+    <NavigationItem link="/auth">sign up</NavigationItem>
   </ul>
 );
 
